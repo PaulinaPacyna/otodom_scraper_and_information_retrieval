25fd622b9df80b686e66e6038177f9524343064e
PaulinaPacyna <pacyna1998@gmail.com>
2024-04-17 21:21:35 +0200

    add table

--- init.sql
+++ init.sql
@@ -4,3 +4,14 @@ CREATE TABLE IF NOT EXISTS apartments (
     description text NOT NULL,
     created_at TIMESTAMP NOT NULL
     );
+
+CREATE TYPE BOOL_WITH_NA AS ENUM ('true', 'false', 'no_information');
+
+CREATE TABLE IF NOT EXISTS retrieved_information (
+    url varchar(100) PRIMARY KEY,
+    long_answer text NOT NULL,
+    mortgage_register BOOL_WITH_NA NOT NULl,
+    lands_regulated BOOL_WITH_NA NOT NULl,
+    rent_administration_fee FLOAT NOT NULl,
+    two_sided BOOL_WITH_NA NOT NULl
+    );
